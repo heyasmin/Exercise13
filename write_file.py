3ca7b9fc4de7338e54b87869b209905245749f2c
Eyasmin Basher <eyasminbasher@gmail.com>
2023-02-10 20:53:04 +0000

    started question 1 ex 13

--- write_file.py
+++ write_file.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python
+
 # Question 1
-# hello
+myfileobject = open("pelican.txt", 'a') #a will append
+afile.write("A wonderful bird is the pelican \n")
+afile.write("His bill holds more than his belican \n")
+lines = ["He can take in his beak,\n", "Enough food for a wekk, \n", "But I'm damned if I see how the helican. \n"]
