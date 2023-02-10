1ce052705e0153db1ca70c9e975f1fc5a7b5d3af
Ellen Houghton <ellen.a.ash@gmail.com>
2023-02-10 20:58:47 +0000

    added writelines

--- write_file.py
+++ write_file.py
@@ -2,6 +2,9 @@
 
 # Question 1
 myfileobject = open("pelican.txt", 'a') #a will append
-afile.write("A wonderful bird is the pelican \n")
-afile.write("His bill holds more than his belican \n")
-lines = ["He can take in his beak,\n", "Enough food for a wekk, \n", "But I'm damned if I see how the helican. \n"]
+myfileobject.write("A wonderful bird is the pelican \n")
+myfileobject.write("His bill holds more than his belican \n")
+lines = ["He can take in his beak,\n", "Enough food for a week, \n", "But I'm damned if I see how the helican. \n"]
+random_variable = myfileobject.writelines(lines)
+
+# The \n creates a newline for each line of the poem.
